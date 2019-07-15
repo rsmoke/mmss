474f5fd1c4dfbca45d017192a0f0fe2bb4ab4be5
Rick Smoke <rsmoke@umich.edu>
2019-07-15 16:00:39 -0400

    Initial Commit

---
+++ Procfile
@@ -0,0 +1,2 @@
+webpack: bin/webpack-dev-server
+web: bin/rails server -p 3000
---
+++ app/controllers/static_pages_controller.rb
@@ -0,0 +1,10 @@
+class StaticPagesController < ApplicationController
+  def index
+  end
+
+  def contact
+  end
+
+  def privacy
+  end
+end
