b9f1f471a9948eda2af827c56535523454a48bb7
Rick Smoke <rsmoke@umich.edu>
2019-09-03 12:02:17 -0400

    UPDATE: add has_many for enrollments

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
   has_one :applicant_detail, dependent: :destroy, inverse_of: :user
+  has_many :enrollments, dependent: :destroy
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
