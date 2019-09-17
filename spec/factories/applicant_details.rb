d4dab5045d91fea19564a5bc0e98d8d413c28b2b
Rick Smoke <rsmoke@umich.edu>
2019-09-17 15:49:35 -0400

    UPDATE: factory syntax

--- spec/factories/applicant_details.rb
+++ spec/factories/applicant_details.rb
@@ -1,33 +1,33 @@
 FactoryBot.define do
   factory :applicant_detail do
     user
-    firstname { "MyString" }
-    middlename { "MyString" }
-    lastname { "MyString" }
-    gender { "MyString" }
-    us_citizen { false }
+    firstname { "Chester" }
+    # middlename { "The" }
+    lastname { "Tester" }
+    gender { "Male" }
+    us_citizen { true }
     demographic { "MyString" }
     birthdate { "2019-07-22" }
-    diet_restrictions { "MyText" }
-    shirt_size { "MyString" }
-    address1 { "MyString" }
-    address2 { "MyString" }
-    city { "MyString" }
-    state { "MyString" }
-    state_non_us { "MyString" }
-    postalcode { "MyString" }
-    country { "MyString" }
-    phone { "MyString" }
-    parentname { "MyString" }
-    parentaddress1 { "MyString" }
-    parentaddress2 { "MyString" }
-    parentcity { "MyString" }
-    parentstate { "MyString" }
-    parentstate_non_us { "MyString" }
-    parentzip { "MyString" }
-    parentcountry { "MyString" }
-    parentphone { "MyString" }
-    parentworkphone { "MyString" }
-    parentemail { "MyString" }
+    diet_restrictions { "peanuts" }
+    shirt_size { "Large" }
+    address1 { "123 Main Street" }
+    # address2 { "MyString" }
+    city { "Saline" }
+    # state { "MyString" }
+    # state_non_us { "MyString" }
+    postalcode { "48896" }
+    country { "US" }
+    phone { "1234567890" }
+    parentname { "Cheryl Stooge" }
+    parentaddress1 { "123 Main Street" }
+    # parentaddress2 { "MyString" }
+    parentcity { "Saline" }
+    # parentstate { "MyString" }
+    # parentstate_non_us { "MyString" }
+    parentzip { "48896" }
+    parentcountry { "US" }
+    parentphone { "9876543217" }
+    # parentworkphone { "MyString" }
+    parentemail { "cheryl.tester@tester.com" }
   end
 end
--- spec/factories/enrollments.rb
+++ spec/factories/enrollments.rb
@@ -1,23 +1,23 @@
 FactoryBot.define do
   factory :enrollment do
-    user { nil }
-    international { false }
-    high_school_name { "MyString" }
-    high_school_address1 { "MyString" }
-    high_school_address2 { "MyString" }
-    high_school_city { "MyString" }
-    high_school_state { "MyString" }
-    high_school_non_us { "MyString" }
-    high_school_postalcode { "MyString" }
-    high_school_country { "MyString" }
-    year_in_school { "MyString" }
-    anticipated_graduation_year { "MyString" }
-    room_mate_request { "MyString" }
-    personal_statement { "MyText" }
-    shirt_size { "MyString" }
-    notes { "MyText" }
-    application_status { "MyString" }
-    offer_status { "MyString" }
-    partner_program { "MyString" }
+    user
+    # international { false }
+    high_school_name { "Academy of Smartness" }
+    high_school_address1 { "345 Main Street" }
+    # high_school_address2 { "MyString" }
+    high_school_city { "Saline" }
+    # high_school_state { "MyString" }
+    # high_school_non_us { "MyString" }
+    # high_school_postalcode { "MyString" }
+    high_school_country { "US" }
+    year_in_school { "Junior" }
+    anticipated_graduation_year { "2018" }
+    # room_mate_request { "MyString" }
+    personal_statement { "I was born in a shed" }
+    # shirt_size { "MyString" }
+    # notes { "MyText" }
+    # application_status { "MyString" }
+    # offer_status { "MyString" }
+    # partner_program { "MyString" }
   end
 end
