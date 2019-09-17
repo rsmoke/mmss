1a40ea63336b43951fc84a5a2755a6a9dc8df64c
Rick Smoke <rsmoke@umich.edu>
2019-09-17 15:44:01 -0400

    UPDATE: validations so they match table

--- app/models/applicant_detail.rb
+++ app/models/applicant_detail.rb
@@ -1,9 +1,10 @@
 class ApplicantDetail < ApplicationRecord
   belongs_to :user, required: true, inverse_of: :applicant_detail
 
-  validates :user_id, presence: true, uniqueness: { case_sensitive: false }
+  # validates :user_id, presence: true, uniqueness: { case_sensitive: false }
   validates :firstname, presence: true
   validates :lastname, presence: true
+  validates :us_citizen, presence: true
   validates :gender, presence: true
   validates :demographic, presence: true
   validates :birthdate, presence: true
@@ -11,7 +12,7 @@ class ApplicantDetail < ApplicationRecord
   validates :shirt_size, presence: true
   validates :address1, presence: true
   validates :city, presence: true
-  validates :state, presence: true
+  # validates :state, presence: true
   validates :postalcode, presence: true
   validates :country, presence: true
   validates :phone, presence: true
@@ -21,6 +22,8 @@ class ApplicantDetail < ApplicationRecord
   validates :parentzip, presence: true
   validates :parentcountry, presence: true
   validates :parentphone, presence: true
+  validates :parentemail, presence: true
+   
 
 
 
