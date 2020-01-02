3a1cbd76e99ab15b13a046a51fa91bc4c0fe5a3b
Rick Smoke <rsmoke@umich.edu>
2020-01-02 18:10:59 -0500

    UPDATE requires and imports to fix layout issue

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,9 +8,10 @@ require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
 require ("css/application.sass")
+require ("css/_footer.sass")
 
-import "../css/application.sass"
-import "../css/_footer.sass"
+// import "../css/application.sass"
+// import "../css/_footer.sass"
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
 // or the `imagePath` JavaScript helper below.
