a76159a4a00b3d387becaf938f0ee3e59ae498ce
Rick Smoke <rsmoke@umich.edu>
2020-01-06 14:16:36 -0500

    UPDATE: element selector to reflect server configuration

--- app/javascript/enrollment/index.js
+++ app/javascript/enrollment/index.js
@@ -1,7 +1,7 @@
 document.addEventListener('DOMContentLoaded', () => {
 
   window.myFunction = function() {
-    var checkBoxSession1 = document.getElementById("enrollment_session_registration_ids_5");
+    var checkBoxSession1 = document.getElementById("enrollment_session_registration_ids_2");
     var activitySession1 = document.getElementById("sessionActivity1");
     var courseSession1 = document.getElementById("sessionCourses1");
     if (checkBoxSession1.checked == true){
@@ -12,7 +12,7 @@ document.addEventListener('DOMContentLoaded', () => {
       courseSession1.style.display = "none";
     }
 
-    var checkBoxSession2 = document.getElementById("enrollment_session_registration_ids_7");
+    var checkBoxSession2 = document.getElementById("enrollment_session_registration_ids_1");
     var activitySession2 = document.getElementById("sessionActivity2");
     var courseSession2 = document.getElementById("sessionCourses2");
     if (checkBoxSession2.checked == true){
@@ -23,7 +23,7 @@ document.addEventListener('DOMContentLoaded', () => {
       courseSession2.style.display = "none";
     }
 
-    var checkBoxSession3 = document.getElementById("enrollment_session_registration_ids_8");
+    var checkBoxSession3 = document.getElementById("enrollment_session_registration_ids_3");
     var activitySession3 = document.getElementById("sessionActivity3");
     var courseSession3 = document.getElementById("sessionCourses3");
     if (checkBoxSession3.checked == true){
