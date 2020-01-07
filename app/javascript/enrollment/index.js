3d10f68295fed7b253c331254f4d2aaa1762ae1e
Rick Smoke <rsmoke@umich.edu>
2020-01-07 11:58:10 -0500

    Fix javascript function call and initiallization

--- app/javascript/enrollment/index.js
+++ app/javascript/enrollment/index.js
@@ -1,9 +1,22 @@
-document.addEventListener('DOMContentLoaded', () => {
+document.addEventListener('turbolinks:load', () => {
+// document.addEventListener('DOMContentLoaded', () => {
 
   window.myFunction = function() {
-    var checkBoxSession1 = document.getElementById("enrollment_session_registration_ids_2");
+    var checkBoxSessionAny = document.getElementById("enrollment_session_registration_ids_9");
+
+    var checkBoxSession1 = document.getElementById("enrollment_session_registration_ids_5");
     var activitySession1 = document.getElementById("sessionActivity1");
     var courseSession1 = document.getElementById("sessionCourses1");
+
+    var checkBoxSession2 = document.getElementById("enrollment_session_registration_ids_7");
+    var activitySession2 = document.getElementById("sessionActivity2");
+    var courseSession2 = document.getElementById("sessionCourses2");
+
+
+    var checkBoxSession3 = document.getElementById("enrollment_session_registration_ids_8");
+    var activitySession3 = document.getElementById("sessionActivity3");
+    var courseSession3 = document.getElementById("sessionCourses3");
+
     if (checkBoxSession1.checked == true){
       activitySession1.style.display = "block";
       courseSession1.style.display = "block";
@@ -12,9 +25,6 @@ document.addEventListener('DOMContentLoaded', () => {
       courseSession1.style.display = "none";
     }
 
-    var checkBoxSession2 = document.getElementById("enrollment_session_registration_ids_1");
-    var activitySession2 = document.getElementById("sessionActivity2");
-    var courseSession2 = document.getElementById("sessionCourses2");
     if (checkBoxSession2.checked == true){
       activitySession2.style.display = "block";
       courseSession2.style.display = "block";
@@ -23,9 +33,6 @@ document.addEventListener('DOMContentLoaded', () => {
       courseSession2.style.display = "none";
     }
 
-    var checkBoxSession3 = document.getElementById("enrollment_session_registration_ids_3");
-    var activitySession3 = document.getElementById("sessionActivity3");
-    var courseSession3 = document.getElementById("sessionCourses3");
     if (checkBoxSession3.checked == true){
       activitySession3.style.display = "block";
       courseSession3.style.display = "block";
@@ -33,8 +40,13 @@ document.addEventListener('DOMContentLoaded', () => {
       activitySession3.style.display = "none";
       courseSession3.style.display = "none";
     }
-  }
 
-window.myFunction;
+    if (checkBoxSessionAny.checked == true){
+      activitySession1.style.display = "block";
+      activitySession2.style.display = "none";
+      activitySession3.style.display = "none";
+
+    }
+  }
 
 })
