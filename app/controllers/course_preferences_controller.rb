da9a84ddb6fe1a51e0011f0736c228dd1e2c8343
Rick Smoke <rsmoke@umich.edu>
2020-06-10 13:39:04 -0400

    update course preferences workflow

--- app/controllers/course_preferences_controller.rb
+++ app/controllers/course_preferences_controller.rb
@@ -34,49 +34,27 @@ class CoursePreferencesController < ApplicationController
   def new
     @current_enrollment = current_user.enrollments.last
     @course_preference = CoursePreference.new
+    @current_enrollment_session1 = @current_enrollment.session_registrations.find_by(description: "Session 1")
+    @current_enrollment_session1_courses = @current_enrollment.course_registrations.where(camp_occurrence: @current_enrollment_session1)
     @current_enrollment_session2 = @current_enrollment.session_registrations.find_by(description: "Session 2")
     @current_enrollment_session2_courses = @current_enrollment.course_registrations.where(camp_occurrence: @current_enrollment_session2)
+    @current_enrollment_session3 = @current_enrollment.session_registrations.find_by(description: "Session 3")
+    @current_enrollment_session3_courses = @current_enrollment.course_registrations.where(camp_occurrence: @current_enrollment_session3)
+
+
   end
 
   def create
-    @current_enrollment = current_user.enrollments.last
-    if @current_enrollment.course_preferences.find_by(course_id: params[:course_preference][:course_id]).present?
-      @current_course_pref_id = @current_enrollment.course_preferences.find_by(course_id: params[:course_preference][:course_id]).id
-      @current_course_pref = @current_enrollment.course_preferences.find(@current_course_pref_id)
-        if params[:course_preference][:ranking] == ""
-          @current_course_pref.delete
-          respond_to do |format|
-            format.html { redirect_to course_preferences_path, notice: 'Course Preference was successfully edited.' }
-            format.json { head :no_content }
-          end
-        else
-          respond_to do |format|
-            if @current_course_pref.update(ranking: params[:course_preference][:ranking])
-            # if @course_preference.save
-              format.html { redirect_to course_preferences_path, notice: 'Course Preference was successfully edited.' }
-              format.json { render :show, status: :ok, location: @course_preference }
-            else
-              format.html { render :edit }
-              format.json { render json: @course_preference.errors, status: :unprocessable_entity }
-            end
-          end
-        end
-    else
-      if params[:course_preference][:ranking] == ""
-        redirect_to enrollment_course_preferences_path(current_user.enrollments.last.id)
-      else
-        @course_preference = @current_enrollment.course_preferences.create(cp_params)
-        respond_to do |format|
-          if @course_preference.save
-            format.html { redirect_to enrollment_course_preferences_path(current_user.enrollments.last.id), notice: 'Course Preference was successfully edited.' }
-            format.json { render :show, status: :created, location: @course_preference }
-          else
-            format.html { render :new }
-            format.json { render json: @course_preference.errors, status: :unprocessable_entity }
-          end
-        end
-      end
-    end
+    redirect_to course_preferences_path, notice: 'Course Preference was successfully edited.' 
+    # respond_to do |format|
+    #   if @current_course_pref.update(ranking: params[:course_preference][:ranking])
+    #     format.html { redirect_to course_preferences_path, notice: 'Course Preference was successfully edited.' }
+    #     format.json { render :show, status: :ok, location: @course_preference }
+    #   else
+    #     format.html { render :edit }
+    #     format.json { render json: @course_preference.errors, status: :unprocessable_entity }
+    #   end
+    # end
   end
 
   def edit
