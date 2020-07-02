45fc82e5ba5084efd2ae307b968bdba958e8ca1a
Rick Smoke <rsmoke@umich.edu>
2020-07-02 17:56:51 -0400

    revert changes to tailwinds config

--- postcss.config.js
+++ postcss.config.js
@@ -1,4 +1,5 @@
-let environment = {
+// var tailwindcss = require('tailwindcss');
+module.exports = {
   plugins: [
     require('postcss-import'),
     require('tailwindcss'),
@@ -9,17 +10,6 @@ let environment = {
         flexbox: 'no-2009'
       },
       stage: 3
-    }),
+    })
   ]
 }
-
-if (process.env.RAILS_ENV === "production") {
-  environment.plugins.push(
-    require('@fullhuman/postcss-purgecss')({
-      content: ['./app/**/*.html.erb', './app/helpers/**/*.rb'],
-      defaultExtractor: content => content.match(/[A-Za-z0-9-_:/]+/g) || []
-    })
-  )
-}
-
-module.exports = environment
