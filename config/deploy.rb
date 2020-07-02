16eb4078943899366d65f7444fcd6fb46ed2bcc9
Rick Smoke <rsmoke@umich.edu>
2020-07-02 15:37:24 -0400

    remove unused cap cofigs

--- config/deploy.rb
+++ config/deploy.rb
@@ -16,9 +16,9 @@ set :puma_workers,    0
 
 # Don't change these unless you know what you're doing
 set :pty,             true
-set :use_sudo,        false
+# set :use_sudo,        false
 set :stage,           :production
-set :deploy_via,      :remote_cache
+# set :deploy_via,      :remote_cache
 set :deploy_to,       "/home/#{fetch(:user)}/apps/#{fetch(:application)}"
 set :puma_bind,       "unix://#{shared_path}/tmp/sockets/#{fetch(:application)}-puma.sock"
 set :puma_state,      "#{shared_path}/tmp/pids/puma.state"
@@ -27,7 +27,7 @@ set :puma_access_log, "#{release_path}/log/puma.error.log"
 set :puma_error_log,  "#{release_path}/log/puma.access.log"
 set :ssh_options,     { forward_agent: true, user: fetch(:user), keys: %w(~/.ssh/id_rsa.pub) }
 set :puma_preload_app, true
-set :puma_worker_timeout, nil
+# set :puma_worker_timeout, nil
 set :puma_init_active_record, true  # Change to false when not using ActiveRecord
 # Avoid permissions issues with using /tmp
 set :tmp_dir, '/home/deployer/tmp'
