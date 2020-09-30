0b92eabcc5691c841e47fadaa684e0e72bafcabf
Rick Smoke <rsmoke@umich.edu>
2020-09-30 16:59:31 -0400

    Add course preferences model into active admin

---
+++ app/admin/course_assignments.rb
@@ -0,0 +1,19 @@
+ActiveAdmin.register CourseAssignment do
+  menu parent: 'Applicant Info', priority: 1
+
+  # See permitted parameters documentation:
+  # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
+  #
+  # Uncomment all parameters which should be permitted for assignment
+  #
+   permit_params :enrollment_id, :course_id
+  #
+  # or
+  #
+  # permit_params do
+  #   permitted = [:enrollment_id, :course_id]
+  #   permitted << :other if params[:action] == 'create' && current_user.admin?
+  #   permitted
+  # end
+  
+end
