e4af38a1c0536b726b599763c3f471bfa73855bd
Rick Smoke <rsmoke@umich.edu>
2020-10-05 17:28:02 -0400

    Deselection of sessin fxnlty
    setup removal of session or course sleection if session was deselected

--- app/javascript/controllers/enrollment_controller.js
+++ app/javascript/controllers/enrollment_controller.js
@@ -5,29 +5,39 @@ export default class extends Controller {
   connect() {
     var checked = this.checkboxTargets.filter(checkbox => checkbox.checked)
     checked.forEach((el) => {
-      console.dir(el.dataset)
-      this.showSession(el.dataset.index)
-      this.showCourses(el.dataset.index)
+      if (el.dataset.index > 0) {
+        this.showSession(el.dataset.index)
+        this.showCourses(el.dataset.index)
+      }
     })
 
   }
   
   display_toggle_session() {
     var item_index = event.currentTarget.dataset.index
-    this.showSession(item_index)
-    this.showCourses(item_index)
+    // alert("current_target: " + item_index);
+    
+    if (item_index > 0) {
+      this.uncheckAll(item_index)
+      this.showSession(item_index)
+      this.showCourses(item_index)
+    }
   }
 
   showSession(index){
-    console.log("Session: " + index)
     this.index = index
-    this.sessionTargets[index].classList.toggle("session--hide")
+    this.sessionTargets[index -1].classList.toggle("session--hide")
   }
 
   showCourses(index){
-    console.log("Course: " + index)
     this.index = index
-    this.coursesTargets[index].classList.toggle("session--hide")
+    this.coursesTargets[index -1].classList.toggle("session--hide")
+  }
+
+  uncheckAll(divid) {
+    $('#sessionActivity' + divid + ' :checkbox:enabled').prop('checked', false);
+    $('#sessionCourses' + divid + ' :checkbox:enabled').prop('checked', false);
   }
 
 }
+
