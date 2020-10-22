737a0ce00d170adb64ce7bad4600153dc0ab2338
Rick Smoke <rsmoke@umich.edu>
2020-10-22 13:43:38 -0400

    temporary work on admin enrollmement

--- app/admin/enrollments.rb
+++ app/admin/enrollments.rb
@@ -15,7 +15,8 @@ ActiveAdmin.register Enrollment, as: "Application" do
                   :room_mate_request, :personal_statement, 
                   :shirt_size, :notes, :application_status, 
                   :offer_status, :partner_program, :transcript,
-                  session_assignments_attributes: [:id, :camp_occurrence_id, :_destroy ],
+                  session_assignment_ids: [],
+                  session_assignments_attributes: [:id, :enrollment_id, :camp_occurrence_id, :_destroy ],
                   course_assignments_attributes: [:id, :course_id, :_destroy ]
   #
   # or
@@ -55,14 +56,15 @@ ActiveAdmin.register Enrollment, as: "Application" do
 
     #  render('/admin/application_session_assignment_form', model: "enrollments")
   
-        f.has_many :session_assignments, heading: 'Session Assignments',
-                    allow_destroy: true,
-                    new_record: false do |a|
+        # f.has_many :session_assignments, heading: 'Session Assignments',
+        #             allow_destroy: true,
+        #             new_record: true do |a|
                       
-                      a.input :id
+        #               a.input :id
 
-                    end
+        #             end
 
+      f.input :session_assignments, as: :check_boxes, collection: application.session_registrations
 
         # f.collection_check_boxes( :session_assignment_ids, application.session_registrations, :id, :description) do |b| 
         #   b.label(class: "inline-flex items-center") do
@@ -70,15 +72,16 @@ ActiveAdmin.register Enrollment, as: "Application" do
         #   end
         # end
 
-
         # render('/admin/application_course_assignment_form', model: "enrollments")
-        f.has_many :course_assignments, heading: 'Course Assignments',
-                    allow_destroy: true,
-                    new_record: false do |a|
+        # f.has_many :course_assignments, heading: 'Course Assignments',
+        #             allow_destroy: true,
+        #             new_record: false do |a|
 
-                      a.input :id 
+        #               a.input :id 
 
-                    end
+        #             end
+
+        # f.input :course_assignments, as: :check_boxes, collection: application.course_preferences
 
         f.input :offer_status, as: :select, collection: ['accepted','declined','offered','none']
         f.input :application_status, as: :select, collection: ['enrolled', 'new', 'none', 'registered', 'submitted']
