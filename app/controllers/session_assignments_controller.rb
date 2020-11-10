7f64bd7ac0078f97410dbf999e005c0d2b4c6623
Rick Smoke <rsmoke@umich.edu>
2020-11-10 13:20:20 -0500

    correct loginc for status updating

--- app/controllers/session_assignments_controller.rb
+++ app/controllers/session_assignments_controller.rb
@@ -4,8 +4,11 @@ class SessionAssignmentsController < InheritedResources::Base
     @session_assignment = SessionAssignment.find(params[:id])
     respond_to do |format|
       if @session_assignment.update(offer_status: "accepted")
-        enroll_id = @session_assignment.enrollment_id
-        Enrollment.find(enroll_id).update(offer_status: "accepted", application_status: "offer accepted")
+        status_array = SessionAssignment.where(enrollment_id: current_enrollment).pluck(:offer_status)
+        if status_array.count("accepted") + status_array.count("declined") == status_array.size
+          enroll_id = @session_assignment.enrollment_id
+          Enrollment.find(enroll_id).update(offer_status: "accepted", application_status: "offer accepted")
+        end
         format.html { redirect_to all_payments_path, notice: 'Session assignment was successfully accepted.' }
         format.json { render :show, status: :ok, location: @session_assignment }
       else
@@ -28,9 +31,18 @@ class SessionAssignmentsController < InheritedResources::Base
         if status_array.count("declined") == status_array.size
           enroll_id = @session_assignment.enrollment_id
           Enrollment.find(enroll_id).update(offer_status: "declined", application_status: "offer declined")
+          format.html { redirect_to root_path, notice: 'Session assignment was declined.' }
+          format.json { render :show, status: :ok, location: @session_assignment }
+        elsif status_array.count("accepted") + status_array.count("declined") == status_array.size
+          enroll_id = @session_assignment.enrollment_id
+          Enrollment.find(enroll_id).update(offer_status: "accepted", application_status: "offer accepted")
+          format.html { redirect_to all_payments_path, notice: 'Session assignment was declined.' }
+          format.json { render :show, status: :ok, location: @session_assignment }
+        else
+          format.html { redirect_to root_path, notice: 'Session assignment was declined.' }
+          format.json { render :show, status: :ok, location: @session_assignment }
         end
-        format.html { redirect_to all_payments_path, notice: 'Session assignment was declined.' }
-        format.json { render :show, status: :ok, location: @session_assignment }
+
       else
         format.html { redirect_to root_path, notice: 'There was a problem processing the offer.' }
         format.json { render json: @session_assignment.errors, status: :unprocessable_entity }
