e66719d8c83a4113a97ef9b4d356297865c6be2c
Rick Smoke <rsmoke@umich.edu>
2020-11-16 12:51:12 -0500

    dumped unused selection item

--- app/helpers/financial_aids_helper.rb
+++ app/helpers/financial_aids_helper.rb
@@ -1,7 +1,6 @@
 module FinancialAidsHelper
     def financial_aid_status
         [
-          ['*Select*', ''],
           ['pending', 'pending'],
           ['awarded', 'awarded'],
           ['rejected', 'rejected'],
