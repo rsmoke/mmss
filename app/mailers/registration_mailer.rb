a03e65fa8bb48ac7cbe0c90b02ce7967d5f6a263
Rick Smoke <rsmoke@umich.edu>
2020-11-24 14:30:07 -0500

    add enrolled email

--- app/mailers/registration_mailer.rb
+++ app/mailers/registration_mailer.rb
@@ -8,4 +8,13 @@ class RegistrationMailer < ApplicationMailer
     @camp_config = CampConfiguration.find_by(active: true)
     mail(to: user.email, subject: "UM MMSS: Confirmation of application")
   end
+
+  def app_enrolled_email(user)
+    @user_detail = user
+    @application = ApplicantDetail.find_by(user_id: user)
+    @url = "https://lsa-math-mmss.miserver.it.umich.edu"
+    @camp_config = CampConfiguration.find_by(active: true)
+    @enrollment = user.enrollments.last
+    mail(to: user.email, subject: "UM MMSS: Your enrollment in the #{@camp_config.display_name} camp is complete")
+  end
 end
