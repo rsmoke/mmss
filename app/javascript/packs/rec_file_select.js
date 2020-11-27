7d1ef35b55b3cbf7bd2e64703921f2629231c6c0
Margarita Barvinok <brita@umich.edu>
2020-11-27 10:40:39 -0500

    Make submit button show when textarea has data

--- app/javascript/packs/rec_file_select.js
+++ app/javascript/packs/rec_file_select.js
@@ -1,20 +1,24 @@
 
-function hidesubmit(){
-  if ($("#recupload_recletter").val() == "" && $("#recupload_letter").val() == "" ) {
+function hidesubmit() {
+  if ($("#recupload_recletter").val() == "" && $("#recupload_letter").val() == "") {
+    // alert("hide");
+    // console.log($("#recupload_letter").val())
     $(':submit').hide();
   } else {
+    // console.log($("#recupload_letter").val())
+    // alert("show");
     $(':submit').show();
   }
 }
 
-  $(function(){
+$(function () {
+  hidesubmit();
+  // Hide submit button if either field is empty
+  $('#recupload_letter').on("change input paste keyup textarea", function () {
     hidesubmit();
-    // Hide submit button if either field is empty
-    $('form input').keyup(function() {
-      hidesubmit();
-    });
-
-    $('input[type="file"]').change(function() {
-      hidesubmit();
-    });
   });
+
+  $('input[type="file"]').change(function () {
+    hidesubmit();
+  });
+});
