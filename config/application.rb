831d8796c9a9cb1721aeb32fa6184398c297d661
Margarita Barvinok <brita@umich.edu>
2020-12-02 12:09:16 -0500

    Set the time zone

--- config/application.rb
+++ config/application.rb
@@ -12,6 +12,7 @@ module Mmss
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
     config.action_mailer.preview_path = "#{Rails.root}/spec/mailers/previews"
+    config.time_zone = 'Eastern Time (US & Canada)'
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
