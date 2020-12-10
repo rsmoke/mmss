50009038866b60e3b99564ae0ebf3edfce1de819
Margarita Barvinok <brita@umich.edu>
2020-12-09 20:51:30 -0500

    Add help message to Any Session checkbox

--- app/javascript/controllers/enrollment_controller.js
+++ app/javascript/controllers/enrollment_controller.js
@@ -1,10 +1,13 @@
-import { Controller } from 'stimulus'; 
+import { Controller } from 'stimulus';
 export default class extends Controller {
-  static targets = [ "session", "checkbox", "courses" ]
+  static targets = ["session", "checkbox", "courses"]
 
   connect() {
     var checked = this.checkboxTargets.filter(checkbox => checkbox.checked)
     checked.forEach((el) => {
+      if (el.dataset.index == 0) {
+        $("#any").css('visibility', 'visible')
+      }
       if (el.dataset.index > 0) {
         this.showSession(el.dataset.index)
         this.showCourses(el.dataset.index)
@@ -12,11 +15,18 @@ export default class extends Controller {
     })
 
   }
-  
+
   display_toggle_session() {
     var item_index = event.currentTarget.dataset.index
     // alert("current_target: " + item_index);
-    
+    if (item_index == 0) {
+      if ($("#any").css('visibility') == 'hidden') {
+        $("#any").css('visibility', 'visible')
+      }
+      else {
+        $("#any").css('visibility', 'hidden')
+      }
+    }
     if (item_index > 0) {
       this.uncheckAll(item_index)
       this.showSession(item_index)
@@ -24,14 +34,14 @@ export default class extends Controller {
     }
   }
 
-  showSession(index){
+  showSession(index) {
     this.index = index
-    this.sessionTargets[index -1].classList.toggle("session--hide")
+    this.sessionTargets[index - 1].classList.toggle("session--hide")
   }
 
-  showCourses(index){
+  showCourses(index) {
     this.index = index
-    this.coursesTargets[index -1].classList.toggle("session--hide")
+    this.coursesTargets[index - 1].classList.toggle("session--hide")
   }
 
   uncheckAll(divid) {
