78fd4f571b24df73710324b55d7fdaa3598890af
Rick Smoke <rsmoke@umich.edu>
2020-12-15 11:34:44 -0500

    added text area to field check

--- app/javascript/packs/required_stars.js
+++ app/javascript/packs/required_stars.js
@@ -2,5 +2,9 @@ $(function () {
   $("input[required]").each(function(index) {
     var id = $(this).attr('id');
     $('label[for="'+id+'"]').append('*');
+  });
+  $("textarea[required]").each(function(index) {
+    var id = $(this).attr('id');
+    $('label[for="'+id+'"]').append('*');
   })
 })
