79c5d4f104bac8c1ef7e32e243e5abbedc28f09a
Rick Smoke <rsmoke@umich.edu>
2020-12-16 14:47:57 -0500

    refactor finaid emails

--- app/models/financial_aid.rb
+++ app/models/financial_aid.rb
@@ -13,8 +13,9 @@
 #  payments_deadline :date
 #
 class FinancialAid < ApplicationRecord
-
-  after_save :send_status_watch_email
+  include ApplicantState
+  
+  after_commit :send_status_watch_email, if: :persisted?
 
   belongs_to :enrollment
 
@@ -42,22 +43,25 @@ class FinancialAid < ApplicationRecord
   end
 
   def send_status_watch_email
+    @current_enrollment = self.enrollment
+
     if self.status == 'awarded'
-      FinaidMailer.fin_aid_awarded_email(self).deliver_now
+      FinaidMailer.fin_aid_awarded_email(self, balance_due).deliver_now
     end
     if self.status == 'rejected'
-      FinaidMailer.fin_aid_rejected_email(self).deliver_now
+      FinaidMailer.fin_aid_rejected_email(self, balance_due).deliver_now
     end
   end
 
   def set_deadline
     return if self.status == 'pending'
     
-    if self.payments_deadline.blank? || self.amount_cents <= 0
+    if self.payments_deadline.blank?
       errors.add(:payments_deadline, "you need to set a date")
     end
 
     if self.amount_cents <= 0
+      return if self.status == 'rejected'
       errors.add(:amount_cents, "you need to set an amount")
     end
   end
