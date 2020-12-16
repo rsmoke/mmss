238ff63a410fe666baf1adb693f9e8f484fbfbf3
Rick Smoke <rsmoke@umich.edu>
2020-12-16 12:22:30 -0500

    added class for activeadmin

---
+++ app/models/payment_state.rb
@@ -0,0 +1,75 @@
+class PaymentState
+  def initialize(enrollment)
+    @curr_enrollment = enrollment
+  end
+# module ApplicantState
+  # extend ActiveSupport::Concern
+  # included do
+  #   before_action :get_current_enrollment
+  # end
+
+  # def get_current_enrollment
+  #   if @curr_enrollment
+  #     @curr_enrollment = @curr_enrollment
+  #   end
+  # end
+
+  def registration_activities 
+    @curr_enrollment.registration_activities.order(camp_occurrence_id: :asc)
+  end
+
+  def session_registrations 
+    @curr_enrollment.session_registrations.order(description: :asc)
+  end
+
+  def current_application_status
+    @curr_enrollment.application_status
+  end
+
+  def finaids
+    @curr_enrollment.financial_aids
+  end
+
+  def has_any_session
+    @session_registrations.pluck(:description).include?("Any Session")
+  end
+
+  def finaids_ttl
+    finaids.pluck(:amount_cents).sum
+  end
+
+  def finaids_awarded_ttl
+    finaids.where(status: "awarded").pluck(:amount_cents).sum
+  end
+
+  def users_current_payments
+    @curr_enrollment.user.payments.current_camp_payments
+  end
+
+  def ttl_paid
+    @curr_enrollment.user.payments.current_camp_payments.where(transaction_status: '1').pluck(:total_amount).map(&:to_i).sum
+  end
+
+  def total_cost
+    cost_sessions_ttl + cost_activities_ttl + CampConfiguration.active_camp_fee_cents
+  end
+
+  def balance_due
+    total_cost.to_i - finaids_awarded_ttl.to_i - ttl_paid.to_i
+  end
+
+  def assigned_sessions_ids 
+    SessionAssignment.where(enrollment_id: @curr_enrollment, offer_status: "accepted").pluck(:camp_occurrence_id)
+  end
+
+  def cost_sessions_ttl
+    # sum of each session use is assigned to 
+    CampOccurrence.where(id: assigned_sessions_ids).pluck(:cost_cents).sum
+  end
+
+  def cost_activities_ttl
+    activity_ids = EnrollmentActivity.where(enrollment_id: @curr_enrollment).pluck(:activity_id)
+    Activity.where(id: activity_ids, camp_occurrence_id: assigned_sessions_ids).pluck(:cost_cents).sum
+  end
+
+end
