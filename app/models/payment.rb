35f2eff1c593ae4f2e701294dc6ac0b3696e5b46
Margarita Barvinok <brita@umich.edu>
2020-12-16 08:01:42 -0500

    Change status to enrolled and send email after creating payment manualy

--- app/models/payment.rb
+++ app/models/payment.rb
@@ -21,7 +21,9 @@
 #  camp_year          :integer
 #
 class Payment < ApplicationRecord
-  # include ApplicantState
+  include ApplicantState
+  before_save :set_current_enrollment
+  after_commit :send_enrolled_letter, if: :persisted?
 
   validates :transaction_id, presence: true, uniqueness: true
   validates :total_amount, presence: true
@@ -30,4 +32,17 @@ class Payment < ApplicationRecord
 
   scope :current_camp_payments, -> { where('camp_year = ? ', CampConfiguration.active_camp_year) }
 
+  private
+
+  def set_current_enrollment
+    @current_enrollment = self.user.enrollments.current_camp_year_applications.last
+  end
+
+  def send_enrolled_letter
+    if balance_due == 0 && @current_enrollment.student_packet.attached?
+      @current_enrollment.update!(application_status: "enrolled")
+      # send enrollment complete email
+      RegistrationMailer.app_enrolled_email(self.user).deliver_now
+    end
+  end
 end
