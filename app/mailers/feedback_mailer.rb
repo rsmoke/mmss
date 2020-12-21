265ad0dd976d20ede1d7a33528a2c83e3db3e92e
Margarita Barvinok <brita@umich.edu>
2020-12-21 17:51:03 -0500

    Fix feedback email

--- app/mailers/feedback_mailer.rb
+++ app/mailers/feedback_mailer.rb
@@ -2,7 +2,7 @@ class FeedbackMailer < ApplicationMailer
   default from: 'MMSS Registration Server - Feedback <mmss-support@umich.edu>'
 
   def feedback_email
-
+    @url = "https://lsa-math-mmss.miserver.it.umich.edu"
     @feedback = params[:feedback]
     @sender = User.find(@feedback.user_id)
     mail(to: 'mmss-support@umich.edu', subject: "Feedback from #{@sender.email}")
