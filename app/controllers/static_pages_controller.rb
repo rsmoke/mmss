029295cd8fe0710713b8e7079cc98ad7ba1ef717
Rick Smoke <rsmoke@umich.edu>
2020-12-22 11:27:39 -0500

    setup camp open close automation

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -7,6 +7,7 @@ class StaticPagesController < ApplicationController
 
   def index
     @application_materials_due_date = CampConfiguration.active_camp_materials_due_date
+    @active_camp_exists = CampConfiguration.active.exists?
   end
 
   def contact
