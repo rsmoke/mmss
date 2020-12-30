a9aec6fe2a7a941cc052597590aa40b60322b84c
Margarita Barvinok <brita@umich.edu>
2020-12-30 14:26:06 -0500

    Add displaying file upload errors in the top area of the form

--- app/admin/enrollments.rb
+++ app/admin/enrollments.rb
@@ -26,7 +26,7 @@ ActiveAdmin.register Enrollment, as: "Application" do
   scope :enrolled
 
   form do |f| # This is a formtastic form builder
-    f.semantic_errors # shows errors on :base
+    f.semantic_errors *f.object.errors.keys # shows errors on :base
     f.inputs do
      f.input :user_id, as: :select, collection: User.all
      f.input :international
