fc269b98e8576aae79a7cd513e8176630920fc76
Rick Smoke <rsmoke@umich.edu>
2021-01-07 09:56:30 -0500

    update validator to allow for comparison of all but current record

--- app/validators/availability_validator.rb
+++ app/validators/availability_validator.rb
@@ -1,7 +1,7 @@
 class AvailabilityValidator < ActiveModel::EachValidator
 
   def validate_each(record, attribute, value)
-    all_notes = Campnote.all
+    all_notes = Campnote.where.not(id: [record])
     date_ranges = all_notes.map { |b| b.opendate..b.closedate }
 
     date_ranges.each do |range|
