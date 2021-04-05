b55a77cc88d8bc8db96c6d77dd26577c18e5699d
Rick Smoke <rsmoke@umich.edu>
2021-04-05 15:25:39 -0400

    added create and update method

--- app/controllers/rejections_controller.rb
+++ app/controllers/rejections_controller.rb
@@ -1,5 +1,37 @@
 class RejectionsController < InheritedResources::Base
 
+  # POST /genders
+  # POST /genders.json
+  def create
+    @gender = Gender.new(gender_params)
+
+    respond_to do |format|
+      if @gender.save
+        format.html { redirect_to @gender, notice: 'Gender was successfully created.' }
+        format.json { render :show, status: :created, location: @gender }
+        Enrollment.find(enrollment_id).update!(application_status: "rejected")
+        
+      else
+        format.html { render :new }
+        format.json { render json: @gender.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # PATCH/PUT /genders/1
+  # PATCH/PUT /genders/1.json
+  def update
+    respond_to do |format|
+      if @gender.update(gender_params)
+        format.html { redirect_to @gender, notice: 'Gender was successfully updated.' }
+        format.json { render :show, status: :ok, location: @gender }
+      else
+        format.html { render :edit }
+        format.json { render json: @gender.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
   private
 
     def rejection_params
